b874e49e3483f8092718830ca318e3a6806a8d05
RishubPatel <rpate133@calpoly.edu>
2024-11-18 13:36:11 -0800

    Improved conciseness of code
    Added error testing for number of entries in operation file line

--- hw4.py
+++ hw4.py
@@ -2,6 +2,7 @@ import data
 import build_data
 import county_demographics
 import copy
+import sys
 
 ''' Testing:
 
@@ -17,11 +18,7 @@ bachelors_gt_60
 '''
 
 global Demographics
-global Unknown_Error
-
-Unknown_Error = False
 Demographics = build_data.get_data()
-File_Name = f"inputs/{input("Enter the file name: ")}.ops"
 
 class Operations:
 
@@ -43,22 +40,16 @@ class Operations:
             print(f"Filter: state == {state_abbreviation} ({len(reduced_demographics)} entries)")
         return(reduced_demographics)
 
-    def filter_gt(demographics: list[data.CountyDemographics], threshold: float, field: str, print_bool = True) -> list[data.CountyDemographics]: #filters demographics by exceeding a threshold value of some field
-        if "." in field:
-            split_field = field.split(".")
-            reduced_demographics = [demographic for demographic in demographics if vars(demographic)[split_field[0].lower()][split_field[1]] > float(threshold)]
-        else:
-            reduced_demographics = [demographic for demographic in demographics if vars(demographic)[field] > threshold]
+    def filter_gt(demographics: list[data.CountyDemographics], threshold: str, field: float, print_bool = True) -> list[data.CountyDemographics]: #filters demographics by exceeding a threshold value of some field
+        split_field = field.split(".")
+        reduced_demographics = [demographic for demographic in demographics if vars(demographic)[split_field[0].lower()][split_field[1]] > threshold]
         if print_bool:
             print(f"Filter: {field} gt {threshold} ({len(reduced_demographics)} entries)")
         return reduced_demographics
 
     def filter_lt(demographics: list[data.CountyDemographics], threshold: float, field: str, print_bool = True) -> list[data.CountyDemographics]: #filters demographics by being below a threshold value of some field
-        if "." in field:
-            split_field = field.split(".")
-            reduced_demographics = [demographic for demographic in demographics if vars(demographic)[split_field[0].lower()][split_field[1]] < float(threshold)]
-        else:
-            reduced_demographics = [demographic for demographic in demographics if vars(demographic)[field] < threshold]
+        split_field = field.split(".")
+        reduced_demographics = [demographic for demographic in demographics if vars(demographic)[split_field[0].lower()][split_field[1]] < threshold]
         if print_bool:
             print(f"Filter: {field} lt {threshold} ({len(reduced_demographics)} entries)")
         return reduced_demographics
@@ -70,17 +61,11 @@ class Operations:
         return total_population
 
     def population(demographics: list[data.CountyDemographics], field: str, print_bool = True) -> list[data.CountyDemographics]: #returns the total supopulation of some field among given counties
-        if "." in field:
-            split_field = field.split(".")
-            if "Persons Below Poverty Level" in split_field or "Education" in split_field or "Ethnicity" in split_field or "Ethnicities" in split_field or "Age" in split_field: #adjust for percentage value
-                total_subpopulation = sum([(vars(county)[split_field[0].lower()][split_field[1]] / 100) * county.population['2014 Population'] for county in demographics])
-            else:
-                total_subpopulation = sum([vars(county)[split_field[0].lower()][split_field[1]] for county in demographics])
-        else:   
-            if "Ethnicity" in field or "Age" in field or "Education" or "Ethnicities" in field: #adjust for percentage value
-                total_subpopulation = sum([(vars(county)[field] / 100) * county.population['2014 Population'] for county in demographics])
-            else:
-                total_subpopulation = sum([vars(county)[field] for county in demographics])
+        split_field = field.split(".")
+        if "Persons Below Poverty Level" in split_field or "Education" in split_field or "Ethnicity" in split_field or "Ethnicities" in split_field or "Age" in split_field: #adjust for percentage value
+            total_subpopulation = sum([(vars(county)[split_field[0].lower()][split_field[1]] / 100) * county.population['2014 Population'] for county in demographics])
+        else:
+            total_subpopulation = sum([vars(county)[split_field[0].lower()][split_field[1]] for county in demographics])
         if print_bool:
             print(f"2014 {field} population: {total_subpopulation}")
         return total_subpopulation
@@ -97,7 +82,6 @@ class Operations:
 
 def process_operations_file(file_name: str) -> None: #Goes through an operations file and executes all of the operations detected
     global Demographics
-    global Unknown_Error
 
     Demographics_Copy = copy.deepcopy(Demographics)
     try:
@@ -114,40 +98,66 @@ def process_operations_file(file_name: str) -> None: #Goes through an operations
         current_line_num += 1
         split_line = "".join(line.split("\n"))
         split_line = split_line.split(":")
-        try: #execute the operation
+        try: #execute the operation: If incorrect number of inputs for the given operation, print that, else do the operation. If some error comes up, print that.
             if not split_line:
                 pass
             elif split_line[0] == "display":
-                Operations.display(Demographics_Copy)
+                if len(split_line) > 1:
+                    print("Invalid operation: Too many entries. -- Line", current_line_num)
+                else:
+                    Operations.display(Demographics_Copy)
             elif split_line[0] == "filter-gt":
-                Demographics_Copy = Operations.filter_gt(Demographics_Copy, split_line[2], split_line[1])
+                if len(split_line) != 3:
+                    print("Invalid operation: Wrong number of entries. -- Line", current_line_num)
+                else:
+                    Demographics_Copy = Operations.filter_gt(Demographics_Copy, float(split_line[2]), split_line[1])
             elif split_line[0] == "filter-lt":
-                Demographics_Copy = Operations.filter_lt(Demographics_Copy, split_line[2], split_line[1])
+                if len(split_line) != 3:
+                    print("Invalid operation: Wrong number of entries. -- Line", current_line_num)
+                else:
+                    Demographics_Copy = Operations.filter_lt(Demographics_Copy, float(split_line[2]), split_line[1])
             elif split_line[0] == "population-total":
-                Operations.population_total(Demographics_Copy)
+                if len(split_line) > 1:
+                    print("Invalid operation: Too many entries. -- Line", current_line_num)
+                else:
+                    Operations.population_total(Demographics_Copy)
             elif split_line[0] == "population":
-                Operations.population(Demographics_Copy, split_line[1])
+                if len(split_line) != 2:
+                    print("Invalid operation: Wrong number of entries. -- Line", current_line_num)
+                else:
+                    Operations.population(Demographics_Copy, split_line[1])
             elif split_line[0] == "percent":
-                Operations.percent(Demographics_Copy, split_line[1])
+                if len(split_line) != 2:
+                    print("Invalid operation: Wrong number of entries. -- Line", current_line_num)
+                else:
+                    Operations.percent(Demographics_Copy, split_line[1])
             elif split_line[0] == "filter-state":
-                Demographics_Copy = Operations.filter_state(Demographics_Copy, split_line[1])
+                if len(split_line) != 2:
+                    print("Invalid operation: Wrong number of entries. -- Line", current_line_num)
+                elif len(split_line[1]) != 2:
+                    print("Invalid state abbreviation -- Line", current_line_num)
+                else:
+                    Demographics_Copy = Operations.filter_state(Demographics_Copy, split_line[1])
             else:
-                print("No operation found -- Line", current_line_num)
-        except IndexError:
-            print("Inputs not found (index error) -- Line", current_line_num)
+                print("No valid operation found -- Line", current_line_num)
         except ValueError:
             print("Invalid input(s) detected -- Line", current_line_num)
         except KeyError:
             print("Invalid input(s) detected -- Line", current_line_num)
         except:
-            print("UNKNOWN ERROR -- Line", current_line_num)
-            Unknown_Error = current_line_num
+            print("Invalid input or no such data exists -- Line", current_line_num)
+    
+    file.close()
 
 if __name__ == "__main__":
-    process_operations_file(File_Name)
-    '''if type(Unknown_Error) != bool:
-        print("#" * 200)
-        print("Unknown Error: ".upper(), Unknown_Error)
-        print("#" * 200)'''
+    #File_Name = f"inputs/{input("No file detected in command-line arguments. Enter the file name: ")}.ops" if len(sys.argv) == 1 else sys.argv[1]
+    try:
+        File_Name = sys.argv[1]
+        process_operations_file(File_Name)
+    except IndexError:
+        print("No file detected in the command-line arguments.")
+        exit()
     #print(vars(Demographics[0]))
     #print(vars(Demographics[1]))
+
+    exit()
